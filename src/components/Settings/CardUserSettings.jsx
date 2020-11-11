d64147344eee3532fdd5c8e3787d05f7b098f2b5
josesrodriguez610 <josesrodriguez610@gmail.com>
2020-11-10 23:56:03 -0600

    (add) User Settings inputs

--- src/components/Settings/CardUserSettings.jsx
+++ src/components/Settings/CardUserSettings.jsx
@@ -1,24 +1,60 @@
 import { Button, Input, Typography } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 
 function CardUserSettings() {
+  const [username, setUsername] = useState('');
+  const [teamName, setTeamName] = useState('');
+  const [teamLogo, setTeamLogo] = useState('');
+
+  const submitUser = () => { };
+  const submitTeam = () => { };
+  const submitLogo = () => { };
+
+  const options = [
+    {
+      name: 'Username',
+      state: username,
+      setState: setUsername,
+      click: submitUser
+    },
+    {
+      name: 'Team Name',
+      state: teamName,
+      setState: setTeamName,
+      click: submitTeam
+    },
+    {
+      name: 'Team Logo',
+      state: teamLogo,
+      setState: setTeamLogo,
+      click: submitLogo
+    }
+  ];
+
   return (
     <Typography>
-      <Input
-        variant='outlined'
-        type='text'
-        value=''
-        onChange=''
-      />
-      <Button
-        variant='contained'
-        color='primary'
-        type='submit'
-        onClick=''
-        disabled=''
-      >
-        add
-      </Button>
+      {options.map(({
+        name, state, setState, click
+      }) => (
+        <>
+          {name}
+          <Input
+            variant='outlined'
+            type='text'
+            value={state}
+            onChange={(e) => setState(e.target.value)}
+          />
+          <Button
+            variant='contained'
+            color='primary'
+            type='submit'
+            onClick={click}
+            disabled=''
+          >
+            add
+          </Button>
+        </>
+      ))}
     </Typography>
   );
 }
