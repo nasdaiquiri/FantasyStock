b8f017fb63a4cc66d24065f03a7e0e293fecd568
josesrodriguez610 <josesrodriguez610@gmail.com>
2020-11-14 17:57:50 -0600

    (add) Open Icon Accordion

--- src/components/AccordionComp.jsx
+++ src/components/AccordionComp.jsx
@@ -2,14 +2,22 @@ import {
   Accordion, AccordionDetails, AccordionSummary, Typography
 } from '@material-ui/core';
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import propTypes from 'prop-types';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles({
+  icon: {
+    color: 'white'
+  }
+});
 
 function AccordionComp({ Component, title }) {
   AccordionComp.propTypes = {
     Component: propTypes.func.isRequired,
     title: propTypes.string.isRequired
   };
-
+  const classes = useStyles();
   const [expanded, setExpanded] = useState(false);
 
   const handleAccordion = (panel) => (event, newExpanded) => {
@@ -22,6 +30,7 @@ function AccordionComp({ Component, title }) {
       onChange={handleAccordion('leagueSettings')}
     >
       <AccordionSummary
+        expandIcon={<ExpandMoreIcon className={classes.icon} />}
         aria-controls='panel1d-content'
         id='panel1d-header'
       >
